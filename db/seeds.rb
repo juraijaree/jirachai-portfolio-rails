57c168d008a501841c22a25ccd2aaf2e0c29f604
JirachaiHuad <jirachai_u@hotmail.com>
2019-05-19 00:14:36 +0700

    implement custom scopes for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+3.times do |topic|
+  Topic.create!(
+    title: "Topic no.#{topic + 1}"
+  )
+end
+
+puts "3 topics created!"
+
 10.times do |blog|
   Blog.create!(
     title: "Blog no.#{blog + 1}",
-    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus auctor, mi ut hendrerit suscipit, neque est ultrices velit, eu pretium nibh enim vitae erat. Aliquam sit amet pulvinar dolor, ut ultrices sapien. Cras varius at nisi ut egestas. Duis sit amet dignissim est. Vivamus commodo ullamcorper tellus quis bibendum. Nunc mauris augue, vestibulum eget gravida dignissim, mollis ac purus. Phasellus vel felis in arcu maximus tincidunt. Sed condimentum eros efficitur ex dapibus, nec porttitor purus tincidunt. Nullam et convallis risus. Proin a ex libero. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque a fringilla nulla, vel viverra dui. Maecenas porttitor laoreet enim, quis sagittis justo posuere sed."
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus auctor, mi ut hendrerit suscipit, neque est ultrices velit, eu pretium nibh enim vitae erat. Aliquam sit amet pulvinar dolor, ut ultrices sapien. Cras varius at nisi ut egestas. Duis sit amet dignissim est. Vivamus commodo ullamcorper tellus quis bibendum. Nunc mauris augue, vestibulum eget gravida dignissim, mollis ac purus. Phasellus vel felis in arcu maximus tincidunt. Sed condimentum eros efficitur ex dapibus, nec porttitor purus tincidunt. Nullam et convallis risus. Proin a ex libero. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque a fringilla nulla, vel viverra dui. Maecenas porttitor laoreet enim, quis sagittis justo posuere sed.",
+    topic_id: Topic.last.id
   )
 end
 
@@ -24,10 +33,20 @@ end
 
 puts "5 skills created!"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
     title: "Porfolio title: #{portfolio_item + 1}",
-    subtitle: "My application",
+    subtitle: "Ruby on Rails",
+    body: "Praesent cursus suscipit pulvinar. Cras condimentum tellus in lacus bibendum, at vestibulum ex tempor. Nulla non fringilla orci, ut commodo eros. Etiam auctor libero tellus, et imperdiet purus luctus ac. Etiam libero turpis, sodales et est eu, malesuada pulvinar urna. Fusce pellentesque sem eros, at consectetur sapien feugiat ut. Etiam vitae luctus neque. Nunc at massa vel tortor congue tempor vitae nec ligula. Sed aliquet gravida venenatis. Mauris eget erat nec odio iaculis accumsan vitae eu ante.",
+    main_image: "https://via.placeholder.com/600x400",
+    thumb_image: "https://via.placeholder.com/350x200"
+  )
+end
+
+1.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Porfolio title: #{portfolio_item + 1}",
+    subtitle: "React",
     body: "Praesent cursus suscipit pulvinar. Cras condimentum tellus in lacus bibendum, at vestibulum ex tempor. Nulla non fringilla orci, ut commodo eros. Etiam auctor libero tellus, et imperdiet purus luctus ac. Etiam libero turpis, sodales et est eu, malesuada pulvinar urna. Fusce pellentesque sem eros, at consectetur sapien feugiat ut. Etiam vitae luctus neque. Nunc at massa vel tortor congue tempor vitae nec ligula. Sed aliquet gravida venenatis. Mauris eget erat nec odio iaculis accumsan vitae eu ante.",
     main_image: "https://via.placeholder.com/600x400",
     thumb_image: "https://via.placeholder.com/350x200"
