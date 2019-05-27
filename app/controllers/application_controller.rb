a79251f047cdcf5fe0cb7a0e572f83fc09d8aafb
JirachaiHuad <jirachai_u@hotmail.com>
2019-05-27 13:07:08 +0700

    add copyright msg

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,3 +4,12 @@ class ApplicationController < ActionController::Base
   include CurrentUserConcern
   include DefaultPageContent
 end
+
+# To be in my own gem
+module JirachaiViewTool
+  class Renderer
+    def self.copyright name, msg
+      "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+    end
+  end
+end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,8 @@ module ApplicationHelper
       content_tag :p, greetin_text, class: "source-greeting"
     end
   end
+
+  def copyright_generator
+    JirachaiViewTool::Renderer.copyright 'Huad', 'All rights reserved'
+  end
 end
